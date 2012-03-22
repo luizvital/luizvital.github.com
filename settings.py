a904da7ce87faf97293b195a394c0cfe782483dc
Luiz Vital <luiz.vital@gmail.com>
2012-03-22 18:13:40 -0300

    Tá vivo?

---
+++ settings.py
@@ -0,0 +1,13 @@
+AUTHOR = 'Luiz Fernando Barbosa Vital'
+DISQUS_SITENAME = 'luizvital'
+GITHUB_URL = 'https://github.com/luizvital/luizvital.github.com'
+GOOGLE_ANALYTICS='UA-30226390-1'
+SITEURL = 'http://luizvital.github.com'
+SITENAME = 'luizvital'
+SOCIAL = (('twitter', 'http://twitter.com/luizvital'),
+          ('github', 'https://github.com/luizvital'),
+          ('facebook', 'http://www.facebook.com/luizvital'),)
+TAG_FEED = 'feeds/%s.atom.xml'
+THEME = 'notmyidea'
+TWITTER_USERNAME = 'luizvital'
+TIMEZONE = 'America/Sao_Paulo'
